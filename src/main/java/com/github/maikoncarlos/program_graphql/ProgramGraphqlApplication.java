b146c30a5b54e504c7594de595640b48f367a541
Maikoncarlos <maikonsaturno@gmail.com>
2024-10-26 18:19:15 -0300

    extruct program graphql

---
+++ src/main/java/com/github/maikoncarlos/program_graphql/ProgramGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.program_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maikoncarlos/program_graphql/ProgramGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.program_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
